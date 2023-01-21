972e99b8ca63381361f83f02d2d0c10eedd3c1d4
lathanaregudam <123231480+lathanaregudam@users.noreply.github.com>
2023-01-21 13:20:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit
-import pandas
+
 streamlit.title('My Parents New Healthy Dinner for today')
 streamlit.header('Lunch Favorites')
 streamlit.text('Roti')
 streamlit.text('Rice,Dal')
 streamlit.text('curd')
 streamlit.text('🍛 🥘 Rice, Pizza')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframes(my_fruit_list)
           
