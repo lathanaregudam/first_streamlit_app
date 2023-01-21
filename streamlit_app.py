ece274d84115654e2cc9158535df213252911b10
lathanaregudam <123231480+lathanaregudam@users.noreply.github.com>
2023-01-21 12:52:53 +0530

    Update streamlit_app.py
    new additions to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner for today')
 streamlit.header('Lunch')
 streamlit.text('Roti')
-streamlit('Rice,dal,curd')
+streamlit.('Rice','dal')
+streamlit.('curd')
           
